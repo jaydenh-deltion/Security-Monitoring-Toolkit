9a6faea6b3df5852e4ebdfc2dad42a004c194212
jaydenh-deltion <161732348+JaydenH04@users.noreply.github.com>
2026-01-28 14:44:52 +0100

    file intergrity with a samt function to notify you if there is a chance from the file that is stored

--- systems/file_integrity.py
+++ systems/file_integrity.py
@@ -12,15 +12,40 @@ def calculate_hash(filepath):
         return None
     
 def monitor_file_integrity(filepath):
-    print(f"\n[*] Monitoring file integrity for: {filepath}\n")
-    current_hash = calculate_hash(filepath)
+    vault_path = "systems/integrity_vault.txt"
+    found = False
 
+    current_hash = calculate_hash(filepath)
     if current_hash is None:
-        print(f" [-] Error: File '{filepath}'not found.")
+        print(f" [-] Error: File '{filepath}' not found.")
         return
     
+    if os.path.exists(vault_path):
+         with open(vault_path, "r" ) as vault_file:
+            for line in vault_file:
+                stored_filepath, stored_hash = line.strip().split(',')
+
+                if stored_filepath == filepath:                  
+                    found = True
+                    if current_hash != stored_hash:
+                            print(f" [!] ALERT: File has been modified! ")
+                            print(f" [+] Old hash: {stored_hash}")
+                            print(f" [+ New hash: {current_hash}]")
+                    else:
+                            print(f" [*] File integrity verified. Hash: {stored_hash}")
+                    return
+
+                    print(f"[*] File already monitored. Stored hash: {stored_hash}")
+                    return
+
+    print(f"\n[*] Monitoring file integrity for: {filepath}")
+    
+
+    with open(vault_path, "a") as vault_file:
+        vault_file.write(f"{filepath},{current_hash}\n")
+    
+    print(f" [+] New baseline created and stored.")
     print(f" [+] Current hash: {current_hash}")
-    print("\n Tip: Copy this hash. If you change the file and run this again, you will see it changes! ")
 
 if __name__ == "__main__":
     test_filepath = input("Enter the file path to monitor: ")
